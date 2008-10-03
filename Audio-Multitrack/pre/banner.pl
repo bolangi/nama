ac48447b82de8b631135285b1cee5235a77ab498
Joel Roth <joelz@pobox.com>
2008-10-04 06:26:24 +0900

    - fixed missing sub

--- Audio-Multitrack/pre/banner.pl
+++ Audio-Multitrack/pre/banner.pl
@@ -2,7 +2,7 @@ print <<BANNER;
 
      /////////////////////////////////////////////////////////////////////
     //                                        / /   /     ///           /
-   // Nama multitrack recorder v. $VERSION                                 /
+   // Nama multitrack recorder v. $VERSION                                /
   /                                    Audio processing by Ecasound 
  /       (c) 2008 Joel Roth                      ////               //
 /////////////////////////////////////////////////////////////////////
