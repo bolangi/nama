6cfe64e765d0a882f7acc96a8eb3c870ff1e22a6
Joel Roth <joelz@pobox.com>
2009-02-14 19:05:38 -1000

    remove_track doesn't try to remove track_gui

--- src/grammar_body.pl
+++ src/grammar_body.pl
@@ -58,7 +58,7 @@ remove_track: _remove_track name end {
 	print("$item{name}: unknown track... skipping.\n"), return
 		if ! defined $track;
 	$track->set(hide => 1); 
-	$::ui->remove_track_gui($track->n);
+	#$::ui->remove_track_gui($track->n);
 	1;
 }
 generate: _generate end { ::generate_setup(); 1}
