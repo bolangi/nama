c38a6cc1401a5cd178a847e6299fd46df3b9a0f2
Joel Roth <joelz@pobox.com>
2009-10-23 10:25:45 -1000

    - bus/rules seem okay

--- src/Core_subs.pl
+++ src/Core_subs.pl
@@ -241,8 +241,8 @@ sub prepare {
 	init_buses();	
 	
 	initialize_rules(); 					# bus/rule routing
-	say join " ", %::Rule::byname;
-	die "here";
+	say join " ", %::Rule::by_name;
+	#die "here";
 	initialize_routing_dispatch_table();	# graph-based routing
 
 	$ui->init_gui;
