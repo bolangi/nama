4ae3477f8972b3d12fb1c7736428c8d79f3733a1
Joel Roth <joelz@pobox.com>
2009-11-01 00:52:08 -1000

    - fix merge

--- src/grammar_body.pl
+++ src/grammar_body.pl
@@ -180,9 +180,12 @@ autofix_tracks: _autofix_tracks { ::command_process("for mon; fixdc; normalize")
 master_on: _master_on end { ::master_on(); 1 }
 master_off: _master_off end { ::master_off(); 1 }
 
-exit: _exit end { 	$::term->rl_deprep_terminal();
-					kill 15, ::ecasound_pid();  	
-					CORE::exit(); 1}
+exit: _exit end {   ::remove_small_wavs();
+                    ::save_state($::state_store_file); 
+                    $::term->rl_deprep_terminal();
+                    kill 15, ::ecasound_pid();      
+                    CORE::exit(); 1}	
+
 source: _source name { print "source with argument$/"; $::this_track->set_source( $item{name} ); 1 }
 source: _source end { 
 	my $source = $::this_track->source;
