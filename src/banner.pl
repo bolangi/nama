d491d6c320f440ee874c3853667f32f1f1d72a8a
Joel Roth <joelz@pobox.com>
2011-04-17 10:26:58 -1000

    update copyright years

--- src/banner.pl
+++ src/banner.pl
@@ -1,7 +1,7 @@
 $banner = <<BANNER;
       ////////////////////////////////////////////////////////////////////
      /                                                                  /
-    /    Nama multitrack recorder v. $VERSION (c)2008-2009 Joel Roth     /
+    /    Nama multitrack recorder v. $VERSION (c)2008-2011 Joel Roth     /
    /                                                                  /
   /    Audio processing by Ecasound, courtesy of Kai Vehmanen        /
  /                                                                  /
