aa546add6aadd9c16a7b43f8e51dc96d04f80491
Joel Roth <joelz@pobox.com>
2013-11-14 16:15:42 -1000

    comment

--- src/singletons.pl
+++ src/singletons.pl
@@ -3,6 +3,10 @@ $mode
 $file
 $graph
 $setup
+# minimally initiated, a new chain setup will overwrite some
+# fields
+# used for status snapshots and last_rec_track lists that
+# require continuity over several setups.
 $config
 $jack
 $fx
