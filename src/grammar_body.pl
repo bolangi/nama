9c8cf9c0bb0907a359b5b7a4f810ab1524e1e3ed
Joel Roth <joelz@pobox.com>
2014-01-16 14:32:14 -1000

    remove unnecessary

--- src/grammar_body.pl
+++ src/grammar_body.pl
@@ -1632,7 +1632,7 @@ remove_from_sequence: _remove_from_sequence position(s) {
 delete_sequence: _delete_sequence existing_sequence_name {
 	$::bn{$item{existing_sequence_name}}->remove
 }
-position: dd { $::this_sequence->verify_item($item{dd}) and $return = $item{dd} }
+position: dd { $::this_sequence->verify_item($item{dd}) and $item{dd} }
 add_spacer: _add_spacer value position {
 	$::this_sequence->new_spacer(
 		duration => $item{value},
