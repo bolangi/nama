bolangi/nama

commit c6644f11f7eae28d8c53fdd74d12c2ed1cc5729d

remove midi terminal mode function, supplanted by midi command detection

src/Grammar.p
@@ -54,12 +54,6 @@ sub process_line {
 		$text->{term}->addhistory($user_input) 
 			unless $user_input eq $text->{previous_cmd} or ! $text->{term};
 		$text->{previous_cmd} = $user_input;
-		if ($mode->{midi_terminal}){
-				$user_input =~ /^\s*(midi_mode_off|mmx)/ 
-					?  process_command($user_input)
-					:  midi_command($user_input);	
-		}
-		else {
 			my $context = context();
 			my $success = process_command( $user_input );
 			my $command_stamp = { context => $context, 
@@ -78,7 +72,6 @@ sub process_line {
 				reconfigure_engine(); # quietly, avoiding noisy reconfig below
 			}
 			reconfigure_engine();
-		}
 		# reset current track to Master if it is
 		# undefined, or the track has been removed
 		# from the index
@@ -91,7 +84,6 @@ sub process_line {
 		logpkg('logcluck',"Inconsistency found in effects data",
 			Dumper ($result)) if $result->{is_error};
 	}
-	revise_prompt( $mode->{midi_terminal} and "Midish > " );
 	my $output = delete $text->{output_buffer};
 }
 sub context {

commit 9c136cbdb61e79caa381e936a0933517125e225c

fix prompt revision broken bug in c6644f11f7e

src/Grammar.p
@@ -85,6 +85,7 @@ sub process_line {
 			Dumper ($result)) if $result->{is_error};
 	}
 	my $output = delete $text->{output_buffer};
+	revise_prompt();
 }
 sub context {
 	my $context = {};