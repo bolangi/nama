ac934340e86f8e1bf844084e61d0b748a33de7c9
Joel Roth <joelz@pobox.com>
2017-08-24 22:09:59 -1000

    introduce Jeanette's set_samplerate command in customize.pl

--- src/customize.pl
+++ src/customize.pl
@@ -34,6 +34,28 @@ commands =>
 				promote_version_to_track($this_track, $v);
 		},
 
+ 		# Change the current project's sample rate
+		# Usage: set_samplerate <samplerate>
+		# Accepted sample rates: 8kHz, 11025Hz, 16kHz, 22.05kHz, 24kHz, 32kHz,
+		# 44.1kHz, 48kHz, 64kHz, 88.2kHz and 96kHz
+		# but you must write them long form: 44100 not 44.1kHz
+		
+		set_samplerate => sub {
+				my ($srate) = @_;
+				my @allowable = qw{ 96000 88200 64000 48000 44100 32000 24000 22050 16000 11025 8000 };
+				my %allowable = map{$_ => 1} @allowable; 
+				if ( $allowable{$srate} ){
+				
+						$project->{sample_rate} = $srate;
+				}
+				else
+				{
+						say("$srate is not an allowed samplerate.");
+						say("Use one of: @allowable");
+				}
+				print ("\n")
+		},
+
 	},
 
 );
