f60febd6e3967668cb325b44215ce7921d7f3228
Joel Roth <joelz@pobox.com>
2023-10-07 14:12:45 -1000

    hotkeys_off command triggers restore_default_keymap()

--- src/grammar_body.pl
+++ src/grammar_body.pl
@@ -1578,6 +1578,7 @@ hotkeys_bump:    _hotkeys_bump  { ::setup_hotkeys('bump' )}
 hotkeys_jump:    _hotkeys_jump  { ::setup_hotkeys('jump' )}
 hotkeys_param:   _hotkeys_param { ::setup_hotkeys('param')}
 hotkeys_list:    _hotkeys_list  { ::list_hotkeys() ; 1 } 
+hotkeys_off:     _hotkeys_off   { ::restore_default_keymap() }
 
 select_sequence: _select_sequence existing_sequence_name { 
 	$::this_sequence = $::bn{$item{existing_sequence_name}}
