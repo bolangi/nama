705af68d89d5173f3f49d0a6dabd2e98eb072d7f
Joel Roth <joelz@pobox.com>
2024-10-29 19:43:49 -1000

    comment out newkey related

--- src/grammar_body.pl
+++ src/grammar_body.pl
@@ -1772,10 +1772,10 @@ bus_off: _bus_off
 }
 
 
-set_param_increment: _set_param_increment value {::set_param_stepize($item{value}    )} 
-set_param_exp:       _set_param_exp       exp   {::set_param_stepize(10**$item{exp});1} 
+#set_param_increment: _set_param_increment value {::set_param_stepize($item{value}    )} 
+#set_param_exp:       _set_param_exp       exp   {::set_param_stepize(10**$item{exp});1} 
 
-set_playback_jumpsize: _set_playback_jumpsize seconds {::set_playback_jumpsize($item{seconds})} 
+#set_playback_jumpsize: _set_playback_jumpsize seconds {::set_playback_jumpsize($item{seconds})} 
 seconds: value
 exp: /[-+]?\d/ 
 # hours:   value 'h' {$item{value}*3600} 
